7f2051d52f1d71df3b31195a8cb132ff9bad12c5
Jonathas Andrade <jonathas.andrade5@gmail.com>
2023-01-29 21:43:25 -0300

    fix: adjust events exports

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,4 @@
-export * from './types/context';
-export * from './types/events';
+export * from './types';
 export * from './useEvent';
 export * from './useEvents';
 export * from './EventsContext';
